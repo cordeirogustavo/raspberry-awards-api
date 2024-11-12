4a7c856678261dbdb83687407292dac3244fafcc
Gustavo Cordeiro <lipule@gmail.com>
2024-11-12 14:14:07 -0300

    fix: rename column from query to return producer

--- src/domain/producer/producer.repository.ts
+++ src/domain/producer/producer.repository.ts
@@ -23,26 +23,26 @@ export class ProducerRepository implements IProducerRepository {
 
         winners_with_intervals AS (
           SELECT 
-              previous_award.producers,
-              previous_award.win_year AS previousWin,
-              following_award.win_year AS followingWin,
-              (following_award.win_year - previous_award.win_year) AS interval
+            previous_award.producers                             AS producer,
+            previous_award.win_year                              AS previousWin,
+            following_award.win_year                             AS followingWin,
+            (following_award.win_year - previous_award.win_year) AS interval
           FROM ranked_wins previous_award
-          INNER JOIN ranked_wins following_award 
-              ON previous_award.producers = following_award.producers
-              AND previous_award.row_num + 1 = following_award.row_num
-          WHERE following_award.win_year - previous_award.win_year > 0
+          INNER JOIN ranked_wins following_award ON previous_award.producers   = following_award.producers AND 
+                                                    previous_award.row_num + 1 = following_award.row_num
+          WHERE 
+            following_award.win_year - previous_award.win_year > 0
         ),
 
         min_max_intervals AS (
           SELECT 
-              MIN(interval) AS min_interval,
-              MAX(interval) AS max_interval
+            MIN(interval) AS min_interval,
+            MAX(interval) AS max_interval
           FROM winners_with_intervals
         )
 
         SELECT 
-          w.producers,
+          w.producer,
           w.previousWin,
           w.followingWin,
           w.interval,
@@ -55,7 +55,7 @@ export class ProducerRepository implements IProducerRepository {
         INNER JOIN min_max_intervals mm 
           ON w.interval = mm.min_interval OR w.interval = mm.max_interval
         ORDER BY 
-          w.producers, 
+          w.producer, 
           w.previousWin;`,
       []
     );
